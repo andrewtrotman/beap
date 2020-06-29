06c54beabe16200c4d0dd6fb5fcbf572e8e47782
AndrewTrotman <andrew@cs.otago.ac.nz>
2020-06-29 15:35:03 +1200

    Initial Commit

---
+++ beap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  beap
+//
+//  Created by andrew on 29/06/20.
+//  Copyright © 2020 andrew. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
